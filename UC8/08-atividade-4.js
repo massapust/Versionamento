25516c73cff640fff99d6ede2530776c122b728d
RAPHAEL BEN-HUR AMARAL MASSAPUST <91230064+massapust@users.noreply.github.com>
2022-11-06 20:30:21 -0300

    ok

---
+++ UC8/08-atividade-4.js
@@ -0,0 +1,18 @@
+        console.log ("Digite sua idade:");
+        console.log ("Digite sua idade:");
+       
+
+        for (i = 0; soma <= 100; i++) {
+            console.log ("Informe sua Idade", idade);
+
+            if (idade >= 18) {
+                soma += i;
+                console.log ("Nome participante", Nome);
+            }
+            else
+            console.log ("Menor de Idade", soma);
+    }
+
+        console.log ("cadastro não será permitido por ter excedido o limite de 100 participantes");
+    
+
